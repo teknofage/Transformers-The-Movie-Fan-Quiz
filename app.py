24e00d5658083a8abcc6157e26388c7fda0b67c8
teknofage <teknofage@gmail.com>
2019-11-13 16:35:12 -0800

    adding framework for three quizzes option

--- app.py
+++ app.py
@@ -1,15 +1,50 @@
-def Quiz():
-    print ("Welcome to the Transformers: The Movie Fan Quiz")
 
-    
+
+transformers = 'Transformers Quiz'
+capitals = 'Capitals Quiz'
+trump_quotes = 'Trump Quotes Quiz'
+
+quizzes = [transformers, capitals, trump_quotes]
+
+def transformers_quiz():
+    print ("Welcome to the Transformers: The Movie Fan Quiz. How much do you really know about the cult classic cartoon?")
+
+    # checks if user input is in correct format
+    guess = input("Guess ONE letter, a-e: ")
+    if len(guess) > 1: # checks if guess is more than one character
+            print (f"Not a valid entry. Please try again. ")
+        if not guess.isalpha(): #checks if guess is an alphabetic letter
+            print (f"Not a valid entry. Please try again. ")
+        #TODO: Check if the guessed letter is in the secret_word or not and give the player feedback
+        if guess == answer
+            score += 1
+            
+        elif not guess.isalpha() or len(guess) > 1: #checks whether guess is not alphabetic, is longer than one character
+            score = score #update score variable 
+        
+
+
+
+def capitals_quiz():
+        print ("Welcome to the Capital Cities of the World Quiz. Test your knowledge of historical and current capital cities.")
+        
         
 
 
 
 
+def trump_quotes_quiz():
+        print ("Welcome to the Trump Quotes Quiz. Choose the missing words from famous (alleged) lies by the former 'Apprentice' reality TV star.")
 
 
-Quiz()
 
-print ("Would you like to play again?")
+topic = raw_input('Do you want to take a quiz on Transformers: The Movie, or capital cities or Trump quotes? Type transformers, cities or Trump.')
+if topic == 'transformers':
+    transformers_quiz()
+elif topic == 'cities':
+    capitals_quiz()
+elif topic == 'trump':
+    trump_quotes_quiz()
+
+print ("Would you like to play again? If so, enter 'yes'.")
 while input() == "yes": Quiz()
